bfcad7ca778ddbc98d7f751062ad9b32d2b4edc9
Dario Cvitanović <dario.cvitanovic04@gmail.com>
2025-08-24 21:52:11 +0200

    Split off tr_target_template.

---
+++ target_template.cmake
@@ -0,0 +1,39 @@
+function(tr_target_template TARGET)
+	set_target_properties (${TARGET} PROPERTIES DEBUG_POSTFIX "d")
+	target_compile_features(${TARGET} PUBLIC cxx_std_20)
+	if((CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT (CMAKE_CXX_SIMULATE_ID STREQUAL "MSVC")) OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+		target_compile_options(${TARGET} PRIVATE
+			-Wall
+			-Wextra
+			-Wpedantic
+			-Wshadow
+			-fno-trapping-math
+			-fno-math-errno
+			-fno-signed-zeros
+			-fassociative-math
+			-ffp-contract=fast
+		)
+		if(NOT CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+			target_link_options(${TARGET} PRIVATE -fuse-ld=lld)
+		endif()
+	elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_SIMULATE_ID STREQUAL "MSVC")
+		target_compile_options(${TARGET} PRIVATE
+			/clang:-Wall
+			/clang:-Wextra
+			/clang:-Wpedantic
+			/clang:-Wshadow
+			/clang:-fno-trapping-math
+			/clang:-fno-math-errno
+			/clang:-fno-signed-zeros
+			/clang:-fassociative-math
+			/clang:-ffp-contract=fast
+		)
+	elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+		target_compile_options(${TARGET} PUBLIC
+			/Zc:preprocessor
+			/W3 
+			/WX
+		)
+	endif()
+	target_compile_definitions(${TARGET} PUBLIC $<$<CONFIG:Debug>:TR_ENABLE_ASSERTS>$<$<CONFIG:RelWithDebInfo>:TR_ENABLE_ASSERTS>)
+endfunction()
