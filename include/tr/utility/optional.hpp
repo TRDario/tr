1c1908f1a6c70f95e7fc66f0e9a0bdb7d238d9ae
Dario Cvitanović <dario.cvitanovic04@gmail.com>
2025-10-18 15:58:12 +0200

    Expand optional.hpp to allow additional arguments.

--- include/tr/utility/optional.hpp
+++ include/tr/utility/optional.hpp
@@ -3,45 +3,47 @@
 
 namespace tr {
 	// Tries to invoke an operation on an optional object if it's non-empty.
-	template <class T, std::invocable<T> Fn> void try_invoke(std::optional<T>& opt, Fn&& fn);
+	template <class T, class... Args, std::invocable<T&, Args...> Fn> void try_invoke(std::optional<T>& opt, Fn&& fn, Args&&... args);
 	// Tries to invoke an operation on an optional object if it's non-empty.
-	template <class T, std::invocable<const T> Fn> void try_invoke(const std::optional<T>& opt, Fn&& fn);
+	template <class T, class... Args, std::invocable<const T&, Args...> Fn>
+	void try_invoke(const std::optional<T>& opt, Fn&& fn, Args&&... args);
 	// Tries to invoke an operation on an optional object if it's non-empty and returns a value.
-	template <class T, class V, std::invocable<T> Fn>
+	template <class T, class... Args, class V, std::invocable<T> Fn>
 		requires(std::same_as<V, std::invoke_result_t<Fn, T&>>)
-	V try_invoke_or(std::optional<T>& opt, Fn&& fn, V fallback);
+	V try_invoke_or(std::optional<T>& opt, Fn&& fn, V fallback, Args&&... args);
 	// Tries to invoke an operation on an optional object if it's non-empty and returns a value.
-	template <class T, class V, std::invocable<const T> Fn>
+	template <class T, class... Args, class V, std::invocable<const T> Fn>
 		requires(std::same_as<V, std::invoke_result_t<Fn, const T&>>)
-	V try_invoke_or(const std::optional<T>& opt, Fn&& fn, V fallback);
+	V try_invoke_or(const std::optional<T>& opt, Fn&& fn, V fallback, Args&&... args);
 } // namespace tr
 
 ////////////////////////////////////////////////////////////// IMPLEMENTATION /////////////////////////////////////////////////////////////
 
-template <class T, std::invocable<T> Fn> void tr::try_invoke(std::optional<T>& opt, Fn&& fn)
+template <class T, class... Args, std::invocable<T&, Args...> Fn> void tr::try_invoke(std::optional<T>& opt, Fn&& fn, Args&&... args)
 {
 	if (opt.has_value()) {
-		std::invoke(std::forward<Fn>(fn), *opt);
+		std::invoke(std::forward<Fn>(fn), *opt, std::forward<Args>(args)...);
 	}
 }
 
-template <class T, std::invocable<const T> Fn> void tr::try_invoke(const std::optional<T>& opt, Fn&& fn)
+template <class T, class... Args, std::invocable<const T&, Args...> Fn>
+void tr::try_invoke(const std::optional<T>& opt, Fn&& fn, Args&&... args)
 {
 	if (opt.has_value()) {
-		std::invoke(std::forward<Fn>(fn), *opt);
+		std::invoke(std::forward<Fn>(fn), *opt, std::forward<Args>(args)...);
 	}
 }
 
-template <class T, class V, std::invocable<T> Fn>
+template <class T, class... Args, class V, std::invocable<T> Fn>
 	requires(std::same_as<V, std::invoke_result_t<Fn, T&>>)
-V tr::try_invoke_or(std::optional<T>& opt, Fn&& fn, V fallback)
+V tr::try_invoke_or(std::optional<T>& opt, Fn&& fn, V fallback, Args&&... args)
 {
-	return opt.has_value() ? std::invoke(std::forward<Fn>(fn), *opt) : std::move(fallback);
+	return opt.has_value() ? std::invoke(std::forward<Fn>(fn), *opt, std::forward<Args>(args)...) : std::move(fallback);
 }
 
-template <class T, class V, std::invocable<const T> Fn>
+template <class T, class... Args, class V, std::invocable<const T> Fn>
 	requires(std::same_as<V, std::invoke_result_t<Fn, const T&>>)
-V tr::try_invoke_or(const std::optional<T>& opt, Fn&& fn, V fallback)
+V tr::try_invoke_or(const std::optional<T>& opt, Fn&& fn, V fallback, Args&&... args)
 {
-	return opt.has_value() ? std::invoke(std::forward<Fn>(fn), *opt) : std::move(fallback);
+	return opt.has_value() ? std::invoke(std::forward<Fn>(fn), *opt, std::forward<Args>(args)...) : std::move(fallback);
 }
