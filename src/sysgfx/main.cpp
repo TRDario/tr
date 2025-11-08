c2ab29fe848707b579a1f755c176afce2ca05546
TRDario <dario.cvitanovic04@gmail.com>
2025-11-08 11:22:34 +0100

    Disable the console on release builds.

--- src/sysgfx/main.cpp
+++ src/sysgfx/main.cpp
@@ -7,6 +7,10 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL_main.h>
 
+#if defined _MSC_VER and not defined TR_ENABLE_ASSERTS
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+
 //
 
 tr::sys::init_error::init_error(std::string_view description)
