2d3ab5fbb374f7c71127860d5f07499e31e752f6
Dario Cvitanović <dario.cvitanovic04@gmail.com>
2025-11-29 10:11:36 +0100

    Add workaround for visible cursor bug in relative mode.

--- src/sysgfx/mouse.cpp
+++ src/sysgfx/mouse.cpp
@@ -3,6 +3,11 @@
 #include "../../include/tr/sysgfx/impl.hpp"
 #include <SDL3/SDL.h>
 
+namespace tr::sys {
+	// Cursor graphic reset timer needed as a workaround for an SDL bug.
+	std::optional<timer> g_cursor_reset_timer;
+} // namespace tr::sys
+
 void tr::sys::set_mouse_relative_mode(bool relative)
 {
 	TR_ASSERT(sdl_window != nullptr, "Tried to set mouse relative mode state before opening the window.");
@@ -10,6 +15,27 @@ void tr::sys::set_mouse_relative_mode(bool relative)
 	if (!SDL_SetWindowRelativeMouseMode(sdl_window, relative)) {
 		TR_LOG(log, tr::severity::ERROR, "Failed to set relative mouse mode.");
 		TR_LOG_CONTINUE(log, "{}", SDL_GetError());
+	}
+
+	if (relative && !g_cursor_reset_timer.has_value()) {
+		// Workaround for an SDL bug where the cursor does not properly disappear sometimes in relative mode.
+		g_cursor_reset_timer.emplace(std::chrono::seconds{1}, [] {
+			float x, y;
+			SDL_GetMouseState(&x, &y);
+			SDL_SetWindowRelativeMouseMode(sdl_window, false);
+			const bool cursor_visible{SDL_CursorVisible()};
+			if (cursor_visible) {
+				SDL_HideCursor();
+			}
+			SDL_WarpMouseInWindow(sdl_window, x, y);
+			if (cursor_visible) {
+				SDL_ShowCursor();
+			}
+			SDL_SetWindowRelativeMouseMode(sdl_window, true);
+		});
+	}
+	else if (!relative) {
+		g_cursor_reset_timer.reset();
 	}
 }
 
