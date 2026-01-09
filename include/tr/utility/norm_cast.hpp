a07b61940d8d7e1eaa54464bf74331ab55e3f58a
TRDario <dario.cvitanovic04@gmail.com>
2026-01-09 01:14:24 +0100

    Document norm_cast.hpp.

--- include/tr/utility/norm_cast.hpp
+++ include/tr/utility/norm_cast.hpp
@@ -1,5 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                                       //
+// Provides a cast between normalized values.                                                                                            //
+//                                                                                                                                       //
+// For integers, their entire range of values can be interpreted as representing fractional values between 0 and 1. tr::norm_cast allows //
+// casting between different types of integers in this format, or converting to or from a floating point value in the range [0, 1].      //
+//     - tr::norm_cast<u8>(i16(0)) -> 128                                                                                                //
+//     - tr::norm_cast<u8>(0.75) -> 191                                                                                                  //
+//     - tr::norm_cast<float>(u8(255)) -> 1.0f                                                                                           //
+//     - tr::norm_cast<float>(0.5) -> 0.5f                                                                                               //
+//                                                                                                                                       //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 #include "macro.hpp"
+
+//////////////////////////////////////////////////////////////// INTERFACE ////////////////////////////////////////////////////////////////
 
 namespace tr {
 	// Converts a floating point number in the range [0-1] to another floating point number in the range [0-1].
@@ -12,7 +27,7 @@ namespace tr {
 	template <std::integral To, std::integral From> constexpr To norm_cast(From from);
 } // namespace tr
 
-/// @cond IMPLEMENTATION
+////////////////////////////////////////////////////////////// IMPLEMENTATION /////////////////////////////////////////////////////////////
 
 template <std::floating_point To, std::floating_point From> constexpr To tr::norm_cast(From from)
 {
@@ -67,5 +82,3 @@ template <std::integral To, std::integral From> constexpr To tr::norm_cast(From 
 		}
 	}
 }
-
-/// @endcond
