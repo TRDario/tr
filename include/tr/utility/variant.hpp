ab2dd8f4ddd0c32d1c95255a3f4b7f078c442c23
TRDario <dario.cvitanovic04@gmail.com>
2026-01-14 22:05:50 +0100

    Overhaul and documentation of variant.hpp.

--- include/tr/utility/variant.hpp
+++ include/tr/utility/variant.hpp
@@ -1,51 +1,97 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                                       //
+// Provides miscellaneous variant-related functionality.                                                                                 //
+//                                                                                                                                       //
+// A number of variant-related concepts are exposed:                                                                                     //
+//     - tr::in_variant<int, std::variant<int, char, float>> -> true                                                                     //
+//     - tr::in_variant<double, std::variant<int, char, float>> -> false                                                                 //
+//     - tr::superset_of<std::variant<int, char, float>, std::variant<int, char>> -> true                                                //
+//     - tr::superset_of<std::variant<int, char>, std::variant<int, char, float>> -> false                                               //
+//     - tr::subset_of<std::variant<int, char>, std::variant<int, char, float>> -> true                                                  //
+//     - tr::subset_of<std::variant<int, char, float>, std::variant<int, char>> -> false                                                 //
+//                                                                                                                                       //
+// A variant can be checked for holding one of a number of types:                                                                        //
+//     - std::variant<int, char, float> v{5.0f}; tr::holds_one_of<int, float>(v) -> true                                                 //
+//                                                                                                                                       //
+// Variants which are subsets/supersets of each other can be cast between. Whether a superset is allowed to be cast with subset_case can //
+// be checked with subset_castable_to:                                                                                                   //
+//     - std::variant<int, char, float> v{5.0f}; tr::subset_castable_to<std::variant<int, char>>(v) -> false                             //
+//     - std::variant<int, char, float> v{5.0f}; tr::subset_castable_to<std::variant<int, float>>(v) -> true                             //
+//     - std::variant<int, char, float> v{5.0f}; tr::subset_cast<std::variant<int, float>>(v) -> std::variant<int, float>{5.0f}          //
+//     - std::variant<int, float> v{5.0f}; tr::superset_cast<std::variant<int, char, float>>(v) -> std::variant<int, char, float>{5.0f}  //
+//                                                                                                                                       //
+// Indices of types in a variant can be gotten through tr::variant_indices:                                                              //
+//     - using idx = tr::variant_indices<std::variant<int, char, float>>                                                                 //
+//       switch (v.index()) {                                                                                                            //
+//       case idx::get<int>:                                                                                                             //
+//           tr::println("Int: {}", tr::get<int>(v));                                                                                    //
+//           break;                                                                                                                      //
+//       case idx::get<char>:                                                                                                            //
+//           tr::println("Char: {}", tr::get<char>(v));                                                                                  //
+//           break;                                                                                                                      //
+//       case idx::get<float>:                                                                                                           //
+//           tr::println("Float: {}", tr::get<float>(v));                                                                                //
+//           break;                                                                                                                      //
+//       }                                                                                                                               //
+//                                                                                                                                       //
+// An unchecked variant getter is provided through tr::get:                                                                              //
+//     - std::variant<int, char, float> v{5.0f}; tr::get<float>(v) -> 5.0f                                                               //
+//     - std::variant<int, std::string> v{"example"}; tr::get<std::string>(std::move(v)) -> rvalue reference to std::string{"example"}   //
+//                                                                                                                                       //
+// Stateless and stateful match classes are provided to support pseudo-match statement syntax. Lambdas passed to the stateful match must //
+// take the state (by value or lvalue reference) as the first parameter. tr::ignore_other_cases or tr::default_result may be used for    //
+// default handling of cases that aren't important:                                                                                      //
+//     - std::variant<int, char, float> v{5.0f}                                                                                          //
+//       v | tr::match{                                                                                                                  //
+//           [] (float v) { tr::println("Float: {}", tr::get<float>(v)); },                                                              //
+//           tr::ignore_other_cases                                                                                                      //
+//       }                                                                                                                               //
+//       -> "Float: 5.0"                                                                                                                 //
+//     - std::variant<int, char, float> v{25}                                                                                            //
+//       v | tr::stateful_match{                                                                                                         //
+//           500,                                                                                                                        //
+//           [] (int& state, int v) { return state + v; },                                                                               //
+//           tr::default_result(42)                                                                                                      //
+//       }                                                                                                                               //
+//       -> 525                                                                                                                          //
+//                                                                                                                                       //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 #include "concepts.hpp"
-#include "macro.hpp"
 
 namespace tr {
-	template <class T, class V> struct type_in_variant : std::false_type {};
-	template <class T, class... Ts>
-		requires(one_of<std::remove_cvref_t<T>, Ts...>)
-	struct type_in_variant<T, std::variant<Ts...>> : std::true_type {};
+	// Determines whether a type is in a variant's type list.
+	template <class T, specialization_of<std::variant> V> struct type_in_variant;
 	// Determines whether a type is in a variant's type list.
 	template <class T, class V>
-	concept in_variant = type_in_variant<T, std::remove_cvref_t<V>>::value;
+	concept in_variant = type_in_variant<T, V>::value;
+	// Gets whether a variant is holding one of a number of types.
+	template <class... Ts, one_of<Ts...>... Us> constexpr bool holds_one_of(const std::variant<Us...>& v);
 
-	template <class T, class V> struct is_superset_of : std::false_type {};
-	template <class... Ts, class... Us>
-		requires(one_of<Us, Ts...> && ...)
-	struct is_superset_of<std::variant<Ts...>, std::variant<Us...>> : std::true_type {};
+	// Determines whether a variant type is a superset of another variant type.
+	template <specialization_of<std::variant> T, specialization_of<std::variant> V> struct is_superset_of;
 	// Determines whether a variant type is a superset of another variant type.
 	template <class T, class V>
-	concept superset_of = is_superset_of<std::remove_cvref_t<T>, std::remove_cvref_t<V>>::value;
+	concept superset_of = is_superset_of<T, V>::value;
 	// Determines whether a variant type is a subset of another variant type.
 	template <class T, class V>
 	concept subset_of = superset_of<V, T>;
-
-	template <class T>
-		requires(specialization_of<T, std::variant>)
-	struct tag_variant {};
-	template <class... Ts> struct tag_variant<std::variant<Ts...>> {
-		using type = std::variant<tag<Ts>...>;
-	};
-	template <class T> using tag_variant_t = tag_variant<T>::type;
-	// Gets the index of a type in a variant.
-	template <class T, class V>
-		requires(in_variant<T, V>)
-	constexpr std::size_t type_index{tag_variant_t<V>(tag<std::remove_cvref_t<T>>{}).index()};
-
-	template <class T, class V> struct subset_castable_to_t {};
-	template <class... Ts, class... Us>
-		requires(subset_of<std::variant<Ts...>, std::variant<Us...>>)
-	struct subset_castable_to_t<std::variant<Ts...>, std::variant<Us...>> {
-		constexpr bool operator()(const std::variant<Us...>& v) const;
-	};
-	// Gets whether a variant is holding one of a number of types.
-	template <class... Ts, class... Us> constexpr bool holds_one_of(const std::variant<Us...>& v);
 	// Gets whether a variant can be casted using subset_cast to another type.
 	template <class T, class V>
 		requires(subset_of<T, V>)
 	constexpr bool subset_castable_to(const V& v);
+	// Casts a variant into another variant that is a superset of it.
+	template <class To, class From>
+		requires(superset_of<To, std::remove_cvref_t<From>>)
+	constexpr To superset_cast(From&& v);
+	// Casts a variant into another variant that is a subset of it.
+	template <class To, class From>
+		requires(subset_of<To, std::remove_cvref_t<From>>)
+	constexpr To subset_cast(From&& v);
+
+	// Struct which can be used to get the indices of types in a variant though get<T>.
+	template <specialization_of<std::variant> T> struct variant_indices;
 
 	// Unchecked variant getter.
 	template <class T, class... Ts> T& get(std::variant<Ts...>& v);
@@ -56,128 +102,36 @@ namespace tr {
 	// Unchecked variant getter.
 	template <class T, class... Ts> const T&& get(const std::variant<Ts...>&& v);
 
-	// Casts a variant into another variant that is a superset of it.
-	template <class To, class From>
-		requires(superset_of<To, From>)
-	constexpr To superset_cast(From&& v);
-	// Casts a variant into another variant that is a subset of it.
-	template <class To, class From>
-		requires(subset_of<To, From>)
-	constexpr To subset_cast(From&& v);
-
 	// Match statement helper class.
 	template <class... Fs> struct match : Fs... {
 		using Fs::operator()...;
 	};
-	// Deduction guide.
-	template <class... Fs> match(Fs...) -> match<Fs...>;
+	// Match statement.
+	template <remove_cvref_specialization_of<std::variant> Variant, remove_cvref_specialization_of<match> Match>
+	decltype(auto) operator|(Variant&& v, Match&& m);
 
-	// Match statement.
-	template <class... Ts, class... Fs> auto operator|(std::variant<Ts...>& v, match<Fs...>&& match);
-	// Match statement.
-	template <class... Ts, class... Fs> auto operator|(std::variant<Ts...>&& v, match<Fs...>&& match);
-	// Match statement.
-	template <class... Ts, class... Fs> auto operator|(const std::variant<Ts...>& v, match<Fs...>&& match);
-	// Match statement.
-	template <class... Ts, class... Fs> auto operator|(const std::variant<Ts...>&& v, match<Fs...>&& match);
+	// Stateful match statement helper class.
+	template <class State, class... Fs> class stateful_match : match<Fs...> {
+	  public:
+		// Constructs a stateful match statement.
+		stateful_match(State&& state, Fs&&... fns);
+		// Calls one of the underlying callables.
+		template <class... Args> decltype(auto) operator()(Args&&... args);
+
+	  private:
+		// The match state.
+		State m_state;
+	};
+	// Stateful match statement.
+	template <remove_cvref_specialization_of<std::variant> Variant, remove_cvref_specialization_of<stateful_match> Match>
+	decltype(auto) operator|(Variant&& v, Match&& m);
+
+	// Used in a match statement to mark all other cases as ignorable.
+	constexpr auto ignore_other_cases{[](auto&&...) {}};
+	// Used in a match statement to give all other cases a default result.
+	template <class T> constexpr auto default_result(T&& value);
 } // namespace tr
 
 ////////////////////////////////////////////////////////////// IMPLEMENTATION /////////////////////////////////////////////////////////////
 
-template <class... Ts, class... Us> constexpr bool tr::holds_one_of(const std::variant<Us...>& v)
-{
-	return (std::holds_alternative<Ts>(v) || ...);
-}
-
-template <class... Ts, class... Us>
-	requires(tr::subset_of<std::variant<Ts...>, std::variant<Us...>>)
-constexpr bool tr::subset_castable_to_t<std::variant<Ts...>, std::variant<Us...>>::operator()(const std::variant<Us...>& v) const
-{
-	return holds_one_of<Ts...>(v);
-}
-
-template <class T, class V>
-	requires(tr::subset_of<T, V>)
-constexpr bool tr::subset_castable_to(const V& v)
-{
-	return subset_castable_to_t<T, V>{}(v);
-}
-
-//
-
-template <class T, class... Args> T& tr::get(std::variant<Args...>& v)
-{
-	TR_ASSERT(std::holds_alternative<T>(v), "Tried to access wrong type on a variant holding the type at index {}.", v.index());
-
-	T* const ptr{std::get_if<T>(&v)};
-	TR_ASSUME(ptr != nullptr);
-	return *ptr;
-}
-
-template <class T, class... Args> T&& tr::get(std::variant<Args...>&& v)
-{
-	TR_ASSERT(std::holds_alternative<T>(v), "Tried to access wrong type on a variant holding the type at index {}.", v.index());
-
-	T* const ptr{std::get_if<T>(&v)};
-	TR_ASSUME(ptr != nullptr);
-	return std::move(*ptr);
-}
-
-template <class T, class... Args> const T& tr::get(const std::variant<Args...>& v)
-{
-	TR_ASSERT(std::holds_alternative<T>(v), "Tried to access wrong type on a variant holding the type at index {}.", v.index());
-
-	const T* const ptr{std::get_if<T>(&v)};
-	TR_ASSUME(ptr != nullptr);
-	return std::move(*ptr);
-}
-
-template <class T, class... Args> const T&& tr::get(const std::variant<Args...>&& v)
-{
-	TR_ASSERT(std::holds_alternative<T>(v), "Tried to access wrong type on a variant holding the type at index {}.", v.index());
-
-	const T* const ptr{std::get_if<T>(&v)};
-	TR_ASSUME(ptr != nullptr);
-	return std::move(*ptr);
-}
-
-//
-
-template <class To, class From>
-	requires(tr::superset_of<To, From>)
-constexpr To tr::superset_cast(From&& v)
-{
-	return std::visit([]<class T>(T&& v) { return To{std::forward<T>(v)}; }, std::forward<From>(v));
-}
-
-template <class To, class From>
-	requires(tr::subset_of<To, From>)
-constexpr To tr::subset_cast(From&& v)
-{
-	TR_ASSERT(subset_castable_to<To>(v), "Tried to subset_cast a variant holding a value of a type not in the subset.");
-
-	return std::visit(tr::match{[]<in_variant<To> T>(T&& v) { return To{std::forward<T>(v)}; }, [](auto&&) -> To { unreachable(); }},
-					  std::forward<From>(v));
-}
-
-//
-
-template <class... Ts, class... Fs> auto tr::operator|(std::variant<Ts...>& v, match<Fs...>&& match)
-{
-	return std::visit(std::move(match), v);
-}
-
-template <class... Ts, class... Fs> auto tr::operator|(std::variant<Ts...>&& v, match<Fs...>&& match)
-{
-	return std::visit(std::move(match), std::move(v));
-}
-
-template <class... Ts, class... Fs> auto tr::operator|(const std::variant<Ts...>& v, match<Fs...>&& match)
-{
-	return std::visit(std::move(match), v);
-}
-
-template <class... Ts, class... Fs> auto tr::operator|(const std::variant<Ts...>&& v, match<Fs...>&& match)
-{
-	return std::visit(std::move(match), std::move(v));
-}
+#include "variant_impl.hpp"
---
+++ include/tr/utility/variant_impl.hpp
@@ -0,0 +1,110 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                                       //
+// Implements variant.hpp.                                                                                                               //
+//                                                                                                                                       //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "macro.hpp"
+#include "variant.hpp"
+
+//////////////////////////////////////////////////////// IN VARIANT / HOLDS ONE OF ////////////////////////////////////////////////////////
+
+template <class T, tr::specialization_of<std::variant> V> struct tr::type_in_variant : std::false_type {};
+
+template <class T, class... Ts>
+	requires(tr::one_of<T, Ts...>)
+struct tr::type_in_variant<T, std::variant<Ts...>> : std::true_type {};
+
+template <class... Ts, tr::one_of<Ts...>... Us> constexpr bool tr::holds_one_of(const std::variant<Us...>& v)
+{
+	return (std::holds_alternative<Ts>(v) || ...);
+}
+
+///////////////////////////////////////////////////////////// SUBSET/SUPERSET /////////////////////////////////////////////////////////////
+
+namespace tr {
+	template <class T, class V> struct subset_castable_to_t;
+} // namespace tr
+
+template <tr::specialization_of<std::variant> T, tr::specialization_of<std::variant> V> struct tr::is_superset_of : std::false_type {};
+
+template <class... Ts, class... Us>
+	requires(tr::one_of<Us, Ts...> && ...)
+struct tr::is_superset_of<std::variant<Ts...>, std::variant<Us...>> : std::true_type {};
+
+template <class... Ts, class... Us> struct tr::subset_castable_to_t<std::variant<Ts...>, std::variant<Us...>> {
+	constexpr bool operator()(const std::variant<Us...>& v) const
+	{
+		return holds_one_of<Ts...>(v);
+	}
+};
+
+template <class T, class V>
+	requires(tr::subset_of<T, V>)
+constexpr bool tr::subset_castable_to(const V& v)
+{
+	return subset_castable_to_t<T, V>{}(v);
+}
+
+///////////////////////////////////////////////////////////////// INDICES /////////////////////////////////////////////////////////////////
+
+template <class... Ts> struct tr::variant_indices<std::variant<Ts...>> {
+	template <class T> static constexpr std::size_t get{std::variant<tag<Ts>...>{tag<T>{}}.index()};
+};
+
+/////////////////////////////////////////////////////////////////// GET ///////////////////////////////////////////////////////////////////
+
+template <class T, class... Args> T& tr::get(std::variant<Args...>& v)
+{
+	TR_ASSERT(std::holds_alternative<T>(v), "Tried to access wrong type on a variant holding the type at index {}.", v.index());
+	return *std::get_if<T>(&v);
+}
+
+template <class T, class... Args> T&& tr::get(std::variant<Args...>&& v)
+{
+	TR_ASSERT(std::holds_alternative<T>(v), "Tried to access wrong type on a variant holding the type at index {}.", v.index());
+	return std::move(*std::get_if<T>(&v));
+}
+
+template <class T, class... Args> const T& tr::get(const std::variant<Args...>& v)
+{
+	TR_ASSERT(std::holds_alternative<T>(v), "Tried to access wrong type on a variant holding the type at index {}.", v.index());
+	return std::move(*std::get_if<T>(&v));
+}
+
+template <class T, class... Args> const T&& tr::get(const std::variant<Args...>&& v)
+{
+	TR_ASSERT(std::holds_alternative<T>(v), "Tried to access wrong type on a variant holding the type at index {}.", v.index());
+	return std::move(*std::get_if<T>(&v));
+}
+
+////////////////////////////////////////////////////////////////// MATCH //////////////////////////////////////////////////////////////////
+
+namespace tr {
+	template <class... Fs> match(Fs...) -> match<Fs...>;
+	template <class State, class... Fs> stateful_match(State&&, Fs&&...) -> stateful_match<State, Fs...>;
+} // namespace tr
+
+template <tr::remove_cvref_specialization_of<std::variant> Variant, tr::remove_cvref_specialization_of<tr::match> Match>
+decltype(auto) tr::operator|(Variant&& v, Match&& m)
+{
+	return std::visit(std::forward<Match>(m), std::forward<Variant>(v));
+}
+
+template <class State, class... Fs> template <class... Args> decltype(auto) tr::stateful_match<State, Fs...>::operator()(Args&&... args)
+{
+	return match<Fs...>::operator()(std::move(m_state), std::forward<Args>(args)...);
+}
+
+template <tr::remove_cvref_specialization_of<std::variant> Variant, tr::remove_cvref_specialization_of<tr::stateful_match> Match>
+decltype(auto) tr::operator|(Variant&& v, Match&& m)
+{
+	return std::visit(std::forward<Match>(m), std::forward<Variant>(v));
+}
+
+////////////////////////////////////////////////////////////// DEFAULT RESULT /////////////////////////////////////////////////////////////
+
+template <class T> constexpr auto tr::default_result(T&& value)
+{
+	return [v = std::move(value)](auto&&...) { return std::move(v); };
+}
