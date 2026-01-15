49dd8f7d39a582f9be585a3726c4c67415835762
TRDario <dario.cvitanovic04@gmail.com>
2026-01-15 23:50:35 +0100

    Document audio.hpp and utility.hpp.

--- include/tr/audio.hpp
+++ include/tr/audio.hpp
@@ -1,3 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                                       //
+// Provides an audio API based on OpenAL.                                                                                                //
+//                                                                                                                                       //
+// Using this module requires the option TR_BUILD_AUDIO to be turned on in CMake. TR_BUILD_AUDIO, in turn, depends on TR_BUILD_SYSGFX.   //
+//                                                                                                                                       //
+// Nothing from this module is allowed to be called in main::parse_command_line.                                                         //
+//                                                                                                                                       //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 #include "audio/buffer.hpp"   // IWYU pragma: export
 #include "audio/listener.hpp" // IWYU pragma: export
