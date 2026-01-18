eeeca1b33ccfee6789d92f79688f4ca0a5ab2f14
TRDario <dario.cvitanovic04@gmail.com>
2026-01-18 17:46:04 +0100

    Document sysgfx.hpp.

--- include/tr/sysgfx.hpp
+++ include/tr/sysgfx.hpp
@@ -1,3 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                                       //
+// Provides a system and graphics API based on SDL3 and OpenGL.                                                                          //
+//                                                                                                                                       //
+// Using this module requires the option TR_BUILD_SYSGFX to be turned on in CMake.                                                       //
+//                                                                                                                                       //
+// When using this module, the standard main entry point is replaced with several custom functions that must be defined, see main.hpp.   //
+//                                                                                                                                       //
+// Most classes and functions in the tr::sys namespace cannot be used in main::parse_command_line. Exceptions to this include the        //
+// functions in dialog.hpp, main.hpp, and path.hpp.                                                                                      //
+//                                                                                                                                       //
+// Classes and functions in the tr::gfx namespace cannot be used while no window is open, and any objects not persist between window     //
+// closings/openings.                                                                                                                    //
+//                                                                                                                                       //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 #include "sysgfx/atlas.hpp"            // IWYU pragma: export
 #include "sysgfx/backbuffer.hpp"       // IWYU pragma: export
