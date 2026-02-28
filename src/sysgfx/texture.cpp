d61be7280d2f8390f01b3aaa077bc15401009b72
TRDario <dario.cvitanovic04@gmail.com>
2026-03-01 00:56:39 +0100

    Fix unpack alignment when loading a texture.

--- src/sysgfx/texture.cpp
+++ src/sysgfx/texture.cpp
@@ -317,6 +317,7 @@ void tr::gfx::texture::set_region(glm::ivec2 tl, const sub_bitmap& bitmap)
 			  "Tried to set out-of-bounds region from ({}, {}) to ({}, {}) in a texture with size {}x{}.", tl.x, tl.y,
 			  tl.x + bitmap.size().x, tl.y + bitmap.size().y, m_size.x, m_size.y);
 
+	TR_GL_CALL(glPixelStorei, GL_UNPACK_ALIGNMENT, 1);
 	TR_GL_CALL(glPixelStorei, GL_UNPACK_ROW_LENGTH, bitmap.pitch() / pixel_bytes(bitmap.format()));
 	TR_GL_CALL(glTextureSubImage2D, m_handle, 0, tl.x, tl.y, bitmap.size().x, bitmap.size().y, format(bitmap.format()),
 			   type(bitmap.format()), bitmap.data());
