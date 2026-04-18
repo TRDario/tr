156d5c3b8911cc53b15a0626fec7130c49eaf7a4
Dario Cvitanović <dario.cvitanovic04@gmail.com>
2026-04-18 10:43:44 +0200

    Remove bounds checks from render target cropping/scissoring.

--- src/sysgfx/render_target.cpp
+++ src/sysgfx/render_target.cpp
@@ -26,23 +26,12 @@ const glm::ivec2& tr::gfx::render_target::size() const
 tr::gfx::render_target tr::gfx::render_target::cropped(const irect2& viewport) const
 {
 	const irect2 absolute_viewport{m_viewport.tl + viewport.tl, viewport.size};
-
-	TR_ASSERT(m_viewport.contains(absolute_viewport),
-			  "Tried to create out-of-bounds sub-target from ({}, {}) to ({}, {}) of a render target of size {}x{}.", viewport.tl.x,
-			  viewport.tl.y, viewport.tl.x + viewport.size.x, viewport.tl.y + viewport.size.y, m_viewport.size.x, m_viewport.size.y);
-
 	return {m_fbo, m_fbo_size, absolute_viewport, absolute_viewport};
 }
 
 tr::gfx::render_target tr::gfx::render_target::scissored(const irect2& scissor_box) const
 {
 	const irect2 absolute_scissor_box{m_viewport.tl + scissor_box.tl, scissor_box.size};
-
-	TR_ASSERT(m_viewport.contains(absolute_scissor_box),
-			  "Tried to create out-of-bounds scissor box from ({}, {}) to ({}, {}) of a render target of size {}x{}.", scissor_box.tl.x,
-			  scissor_box.tl.y, scissor_box.tl.x + scissor_box.size.x, scissor_box.tl.y + scissor_box.size.y, m_viewport.size.x,
-			  m_viewport.size.y);
-
 	return {m_fbo, m_fbo_size, m_viewport, absolute_scissor_box};
 }
 
@@ -50,14 +39,5 @@ tr::gfx::render_target tr::gfx::render_target::subtarget(const irect2& viewport,
 {
 	const irect2 absolute_viewport{m_viewport.tl + viewport.tl, viewport.size};
 	const irect2 absolute_scissor_box{absolute_viewport.tl + scissor_box.tl, scissor_box.size};
-
-	TR_ASSERT(m_viewport.contains(absolute_viewport),
-			  "Tried to create out-of-bounds sub-target from ({}, {}) to ({}, {}) of a render target of size {}x{}.", viewport.tl.x,
-			  viewport.tl.y, viewport.tl.x + viewport.size.x, viewport.tl.y + viewport.size.y, m_viewport.size.x, m_viewport.size.y);
-	TR_ASSERT(absolute_viewport.contains(absolute_scissor_box),
-			  "Tried to create out-of-bounds scissor box from ({}, {}) to ({}, {}) of a render target of size {}x{}.", scissor_box.tl.x,
-			  scissor_box.tl.y, scissor_box.tl.x + scissor_box.size.x, scissor_box.tl.y + scissor_box.size.y, viewport.size.x,
-			  viewport.size.y);
-
 	return {m_fbo, m_fbo_size, absolute_viewport, absolute_scissor_box};
 }
