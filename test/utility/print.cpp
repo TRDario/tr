b4a3c17cbd7bbe1dac2430cc93eb02729e6de707
TRDario <dario.cvitanovic04@gmail.com>
2026-07-04 18:06:38 +0200

    Add defer and print tests.

---
+++ test/utility/print.cpp
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                                       //
+// Tests utility/print.hpp.                                                                                                              //
+//                                                                                                                                       //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <gtest/gtest.h>
+#include <tr/utility/print.hpp>
+
+TEST(print_test, print_to)
+{
+	std::ostringstream os;
+	tr::print_to(os, "PI = {:.2f}", std::numbers::pi);
+	ASSERT_EQ(os.str(), "PI = 3.14");
+}
+
+TEST(print_test, println_to)
+{
+	std::ostringstream os;
+	tr::println_to(os, "PI = {:.2f}", std::numbers::pi);
+	ASSERT_EQ(os.str(), "PI = 3.14\n");
+}
