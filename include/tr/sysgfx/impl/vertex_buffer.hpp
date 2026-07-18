e96b1a316cb05b27668197fb004e1b4292560675
Dario Cvitanović <dario.cvitanovic04@gmail.com>
2026-07-18 09:47:13 +0200

    Clean up vertex_buffer documentation.

--- include/tr/sysgfx/impl/vertex_buffer.hpp
+++ include/tr/sysgfx/impl/vertex_buffer.hpp
@@ -10,43 +10,45 @@
 
 /////////////////////////////////////////////////////////// STATIC VERTEX BUFFER //////////////////////////////////////////////////////////
 
-template <tr::standard_layout T>
-template <tr::typed_contiguous_const_range<T> R>
-tr::static_vertex_buffer<T>::static_vertex_buffer(graphics_context& context, R&& range)
+template <tr::standard_layout Element>
+template <tr::typed_contiguous_const_range<Element> Range>
+tr::static_vertex_buffer<Element>::static_vertex_buffer(graphics_context& context, Range&& range)
 	: basic_static_vertex_buffer{context, range_bytes(range)}
 {
 }
 
 ////////////////////////////////////////////////////////// DYNAMIC VERTEX BUFFER //////////////////////////////////////////////////////////
 
-template <tr::standard_layout T> tr::usize tr::dyn_vertex_buffer<T>::size() const
+template <tr::standard_layout Element> tr::usize tr::dyn_vertex_buffer<Element>::size() const
 {
-	return basic_dyn_vertex_buffer::size() / sizeof(T);
+	return basic_dyn_vertex_buffer::size() / sizeof(Element);
 }
 
-template <tr::standard_layout T> tr::usize tr::dyn_vertex_buffer<T>::capacity() const
+template <tr::standard_layout Element> tr::usize tr::dyn_vertex_buffer<Element>::capacity() const
 {
-	return basic_dyn_vertex_buffer::capacity() / sizeof(T);
+	return basic_dyn_vertex_buffer::capacity() / sizeof(Element);
 }
 
-template <tr::standard_layout T> void tr::dyn_vertex_buffer<T>::resize(usize size)
+template <tr::standard_layout Element> void tr::dyn_vertex_buffer<Element>::resize(usize size)
 {
-	basic_dyn_vertex_buffer::resize(size * sizeof(T));
+	basic_dyn_vertex_buffer::resize(size * sizeof(Element));
 }
 
-template <tr::standard_layout T> void tr::dyn_vertex_buffer<T>::reserve(usize size)
+template <tr::standard_layout Element> void tr::dyn_vertex_buffer<Element>::reserve(usize size)
 {
-	basic_dyn_vertex_buffer::reserve(size * sizeof(T));
+	basic_dyn_vertex_buffer::reserve(size * sizeof(Element));
 }
 
-template <tr::standard_layout T> template <tr::typed_contiguous_const_range<T> R> void tr::dyn_vertex_buffer<T>::set(R&& data)
+template <tr::standard_layout Element>
+template <tr::typed_contiguous_const_range<Element> Range>
+void tr::dyn_vertex_buffer<Element>::set(Range&& data)
 {
 	basic_dyn_vertex_buffer::set(range_bytes(data));
 }
 
-template <tr::standard_layout T>
-template <tr::typed_contiguous_const_range<T> R>
-void tr::dyn_vertex_buffer<T>::set_region(usize offset, R&& data)
+template <tr::standard_layout Element>
+template <tr::typed_contiguous_const_range<Element> Range>
+void tr::dyn_vertex_buffer<Element>::set_region(usize offset, Range&& data)
 {
-	basic_dyn_vertex_buffer::set_region(offset * sizeof(T), range_bytes(data));
+	basic_dyn_vertex_buffer::set_region(offset * sizeof(Element), range_bytes(data));
 }
--- include/tr/sysgfx/vertex_buffer.hpp
+++ include/tr/sysgfx/vertex_buffer.hpp
@@ -6,13 +6,13 @@
 //                                                                                                                                       //
 // The vertex buffer comes in two variants: static vertex buffers are initialized once and are immutable, while dynamic vertex buffers   //
 // can be resized and modified at will. An untyped (prefixed with basic_) and typed variant of each of these is provided:                //
-//     - tr::gfx::basic_static_vertex_buffer -> untyped static vertex buffer                                                             //
-//     - tr::gfx::static_vertex_buffer<T> -> typed static vertex buffer                                                                  //
-//     - tr::gfx::basic_dyn_vertex_buffer -> untyped dynamic vertex buffer                                                               //
-//     - tr::gfx::dyn_vertex_buffer<T> -> typed dynamic vertex buffer                                                                    //
+//     - tr::basic_static_vertex_buffer -> untyped static vertex buffer                                                                  //
+//     - tr::static_vertex_buffer<Element> -> typed static vertex buffer                                                                 //
+//     - tr::basic_dyn_vertex_buffer -> untyped dynamic vertex buffer                                                                    //
+//     - tr::dyn_vertex_buffer<Element> -> typed dynamic vertex buffer                                                                   //
 //                                                                                                                                       //
 // Static vertex buffers are constructed with a span of data that will be copied into the buffer:                                        //
-//     - tr::gfx::static_vertex_buffer<glm::vec2> buffer{data} -> copies 'data' into the buffer                                          //
+//     - tr::static_vertex_buffer<glm::vec2> buffer{data} -> copies 'data' into the buffer                                               //
 //                                                                                                                                       //
 // Dynamic vertex buffers are constructed empty. Whether a buffer is empty can be checked with the .empty() method. Much like            //
 // std::vector, dynamic vertex buffers distinguish between buffer size and capacity, both of which can be queried with the respective    //
@@ -21,15 +21,15 @@
 // with .resize() or set to a copy of a span with .set(), which automatically sets its size to match that of the copied buffer.          //
 // The .set_region() method can be used to set a region of the buffer; this function never affects the size or capacity of the buffer.   //
 // The buffer can be cleared with the .clear() method, but like with std::vector, the allocated capacity is retained:                    //
-//     - tr::gfx::dyn_vertex_buffer<glm::vec2> buffer; buffer.empty() -> true                                                            //
+//     - tr::dyn_vertex_buffer<glm::vec2> buffer; buffer.empty() -> true                                                                 //
 //     - buffer.reserve(100); buffer.resize(50) -> buffer now has size 50, capacity 128                                                  //
 //     - buffer.size(); buffer.capacity() -> 50, 128                                                                                     //
 //     - std::array<glm::vec2, 500> data; buffer.set(data) -> buffer now stores a copy of 'data', has size 500, capacity 512             //
 //     - std::array<glm::vec2, 100> data2; buffer.set_region(400, data2) -> a copy of data2 is now in buffer[400-499]                    //
 //     - buffer.clear() -> buffer now has size 0, capacity 512                                                                           //
 //                                                                                                                                       //
-// The label of a vertex buffer can be set with .set_label():                                                                            //
-//     - vbuf.set_label("Example buffer") -> 'vbuf' is now labelled "Example buffer"                                                     //
+// The label of a vertex buffer can be set with .set_label() and gotten with .label():                                                   //
+//     - vbuf.set_label("Example buffer"); vbuf.label() -> "Example buffer"                                                              //
 //                                                                                                                                       //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -62,10 +62,10 @@ namespace tr {
 	};
 
 	// Typed static vertex buffer class for holding immutable vertex data of a single type.
-	template <standard_layout T> class static_vertex_buffer : private basic_static_vertex_buffer {
+	template <standard_layout Element> class static_vertex_buffer : private basic_static_vertex_buffer {
 	  public:
 		// Creates a static vertex buffer.
-		template <typed_contiguous_const_range<T> R> static_vertex_buffer(graphics_context& context, R&& range);
+		template <typed_contiguous_const_range<Element> Range> static_vertex_buffer(graphics_context& context, Range&& range);
 
 		// Gets a reference to the graphics context the buffer is on.
 		using basic_static_vertex_buffer::context;
@@ -118,7 +118,7 @@ namespace tr {
 	};
 
 	// Typed dynamic vertex buffer class.
-	template <standard_layout T> class dyn_vertex_buffer : private basic_dyn_vertex_buffer {
+	template <standard_layout Element> class dyn_vertex_buffer : private basic_dyn_vertex_buffer {
 	  public:
 		using basic_dyn_vertex_buffer::basic_dyn_vertex_buffer;
 
@@ -139,9 +139,9 @@ namespace tr {
 		// Clears the buffer and guarantees a certain capacity for it.
 		void reserve(usize capacity);
 		// Sets the contents of the buffer, potentially reallocating it.
-		template <typed_contiguous_const_range<T> R> void set(R&& data);
+		template <typed_contiguous_const_range<Element> Range> void set(Range&& data);
 		// Sets a region of the buffer.
-		template <typed_contiguous_const_range<T> R> void set_region(usize offset, R&& data);
+		template <typed_contiguous_const_range<Element> Range> void set_region(usize offset, Range&& data);
 
 		// Gets the debug label of the vertex buffer.
 		using basic_dyn_vertex_buffer::label;
