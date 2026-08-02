a66b0837efb0641ed1baf7a0e9efbfd54f9ccb26
TRDario <dario.cvitanovic04@gmail.com>
2026-08-03 00:45:03 +0200

    Document concepts.

--- include/tr/utility/concepts.hpp
+++ include/tr/utility/concepts.hpp
@@ -1,87 +1,115 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                                                                       //
-// Provides concepts.                                                                                                                    //
-//                                                                                                                                       //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file
+/// Provides concepts.
 
 #pragma once
 #include "template.hpp"
 
-namespace tr {
-	// Concept denoting an arithmetic type.
+namespace tr
+{
+	/// Built-in arithmetic type.
 	template <typename T>
 	concept arithmetic = std::is_arithmetic_v<T>;
-	// Concept denoting an enumerator type.
+
+	/// Enumerator type.
 	template <typename T>
 	concept enumerator = std::is_enum_v<T>;
 
-	// Concept denoting an object type (i.e. not a reference).
+	/// Object (non-reference) type.
 	template <typename T>
 	concept object = std::is_object_v<T>;
-	// Concept denoting a reference.
+
+	/// Pointer type.
+	template <typename T>
+	concept pointer = std::is_pointer_v<T>;
+
+	/// Reference type.
 	template <typename T>
 	concept reference = std::is_reference_v<T>;
-	// Concept denoting an lvalue reference.
+
+	/// Lvalue reference type.
 	template <typename T>
 	concept lvalue_reference = std::is_lvalue_reference_v<T>;
-	// Concept denoting an rvalue reference.
+
+	/// Rvalue reference type.
 	template <typename T>
 	concept rvalue_reference = std::is_rvalue_reference_v<T>;
 
-	// Concept denoting a const-qualified type.
+	/// Const-qualified type.
 	template <typename T>
 	concept const_qualified = std::is_const_v<std::remove_reference_t<T>>;
-	// Concept denoting a volatile-qualified type.
+
+	/// Volatile-qualified type.
 	template <typename T>
 	concept volatile_qualified = std::is_volatile_v<std::remove_reference_t<T>>;
-	// Concept denoting a cv-unqualified type.
+
+	/// CV-unqualified type.
 	template <typename T>
 	concept cv_unqualified = !const_qualified<T> && !volatile_qualified<T>;
-	// Concept denoting a cv-unqualified object.
+
+	/// CV-unqualified object type.
 	template <typename T>
 	concept cv_unqualified_object = object<T> && cv_unqualified<T>;
 
-	// Concept denoting a non-constant lvalue reference.
+	/// Non-constant lvalue reference type.
 	template <typename T>
 	concept non_const_lvalue_reference = lvalue_reference<T> && !const_qualified<T>;
-	// Concept denoting a constant lvalue reference.
+
+	/// Constant lvalue reference type.
 	template <typename T>
 	concept const_lvalue_reference = lvalue_reference<T> && const_qualified<T>;
 
-	// Concept denoting a move-assignable type.
+	//
+
+	/// Move-assignable type.
 	template <typename T>
 	concept move_assignable = std::is_move_assignable_v<T>;
-	// Concept denoting a standard layout type.
+
+	/// Standard layout type.
 	template <typename T>
 	concept standard_layout = std::is_standard_layout_v<T>;
-	// Concept denoting a contiguous range of standard layout objects.
-	template <typename Range>
-	concept standard_layout_range = std::ranges::contiguous_range<Range> && standard_layout<std::ranges::range_value_t<Range>>;
-	// Concept denoting a borrowed contiguous range of standard layout objects.
-	template <typename Range>
-	concept borrowed_standard_layout_range = standard_layout_range<Range> && std::ranges::borrowed_range<Range>;
-	// Concept denoting a borrowed mutable contiguous range of standard layout objects.
-	template <typename Range>
-	concept borrowed_mutable_standard_layout_range = borrowed_standard_layout_range<Range> &&
-													 !const_qualified<std::ranges::range_reference_t<Range>>;
-	// Concept denoting a sized output range.
-	template <typename Range, typename Element>
-	concept sized_output_range = std::ranges::sized_range<Range> && std::output_iterator<std::ranges::iterator_t<Range>, Element>;
-	// Concept denoting an immutable contiguous range whose elements are of a certain type.
-	template <typename Range, typename Element>
+
+	//
+
+	/// Contiguous range of standard layout objects.
+	template <typename T>
+	concept standard_layout_range = std::ranges::contiguous_range<T> && standard_layout<std::ranges::range_value_t<T>>;
+
+	/// Borrowed contiguous range of standard layout objects.
+	template <typename T>
+	concept borrowed_standard_layout_range = standard_layout_range<T> && std::ranges::borrowed_range<T>;
+
+	/// Borrowed mutable contiguous range of standard layout objects.
+	template <typename T>
+	concept borrowed_mutable_standard_layout_range = borrowed_standard_layout_range<T> &&
+													 !const_qualified<std::ranges::range_reference_t<T>>;
+
+	/// Sized output range type.
+	/// @tparam Element Required output type.
+	template <typename T, typename Element>
+	concept sized_output_range = std::ranges::sized_range<T> && std::output_iterator<std::ranges::iterator_t<T>, Element>;
+
+	/// Immutable contiguous range whose elements are of a certain type.
+	/// @tparam Element Required element type.
+	template <typename T, typename Element>
 	concept typed_contiguous_const_range =
-		std::ranges::contiguous_range<Range> &&
-		std::same_as<const Element, std::add_const_t<std::remove_reference_t<std::ranges::range_reference_t<Range>>>>;
-	// Concept denoting a borrowed immutable contiguous range whose elements are of a certain type.
-	template <typename Range, typename Element>
-	concept borrowed_typed_contiguous_const_range = typed_contiguous_const_range<Range, Element> && std::ranges::borrowed_range<Range>;
-	// Concept denoting a mutable contiguous range whose elements are of a certain type.
-	template <typename Range, typename Element>
-	concept typed_contiguous_mutable_range = std::ranges::contiguous_range<Range> &&
-											 std::same_as<Element, std::remove_reference_t<std::ranges::range_reference_t<Range>>>;
-	// Concept denoting a borrowed mutable contiguous range whose elements are of a certain type.
-	template <typename Range, typename Element>
-	concept borrowed_typed_contiguous_mutable_range = typed_contiguous_mutable_range<Range, Element> && std::ranges::borrowed_range<Range>;
+		std::ranges::contiguous_range<T> &&
+		std::same_as<const Element, std::add_const_t<std::remove_reference_t<std::ranges::range_reference_t<T>>>>;
+
+	/// Borrowed immutable contiguous range whose elements are of a certain type.
+	/// @tparam Element Required element type.
+	template <typename T, typename Element>
+	concept borrowed_typed_contiguous_const_range = typed_contiguous_const_range<T, Element> && std::ranges::borrowed_range<T>;
+
+	/// Mutable contiguous range whose elements are of a certain type.
+	/// @tparam Element Required element type.
+	template <typename T, typename Element>
+	concept typed_contiguous_mutable_range = std::ranges::contiguous_range<T> &&
+											 std::same_as<Element, std::remove_reference_t<std::ranges::range_reference_t<T>>>;
+
+	/// Borrowed mutable contiguous range whose elements are of a certain type.
+	/// @tparam Element Required element type.
+	template <typename T, typename Element>
+	concept borrowed_typed_contiguous_mutable_range = typed_contiguous_mutable_range<T, Element> && std::ranges::borrowed_range<T>;
 
 	//
 
@@ -90,32 +118,50 @@ namespace tr {
 	template <typename T, typename... Ts>
 	concept one_of = (std::same_as<T, Ts> || ...);
 
-	// Concept checking whether a type is formattable.
+	//
+
+	/// Formattable type.
+	/// @tparam Context Formatting context type.
+	/// @tparam Formatter Formatter type.
 	template <typename T, typename Context, typename Formatter = typename Context::template formatter_type<std::remove_const_t<T>>>
 	concept formattable_with = std::semiregular<Formatter> && requires(Formatter& f, Formatter const& cf, T&& t, Context fc,
 																	   std::basic_format_parse_context<typename Context::char_type> pc) {
 		{ f.parse(pc) } -> std::same_as<typename decltype(pc)::iterator>;
 		{ cf.format(t, fc) } -> std::same_as<typename Context::iterator>;
 	};
-	// Concept checking whether a type is formattable.
+
+	/// Formattable type.
+	/// @tparam CharT Output character type.
 	template <typename T, typename CharT = char>
 	concept formattable = formattable_with<std::remove_reference_t<T>, std::basic_format_context<CharT*, CharT>>;
-	// Concept checking whether a format string is valid for a set of types.
+
+	/// Format string that is valid for a set of types.
+	/// @tparam Args List of arguments to the formatting function.
 	template <string_literal Fmt, typename... Args>
 	concept valid_format_string_for = (formattable<Args, char> && ...) &&
 									  requires { typename std::integral_constant<int, (std::format_string<Args...>(Fmt.data), 1)>; };
 
-	// Concept denoting a hasher for a specific type.
+	//
+
+	/// Hasher compatible with a specific type.
+	/// @tparam Hashed Type that must be hashable.
 	template <typename T, typename Hashed>
 	concept hasher = requires(const Hashed& v) {
 		{ T{}(v) } -> std::convertible_to<u64>;
 	};
-	// Concept denoting an equality predicate for a specific type.
-	template <typename T, typename L, typename R = L>
-	concept equality_predicate = requires(const L& l, const R& r) {
-		{ T{}(l, r) } -> std::same_as<bool>;
+
+	/// Equality predicate compatible with a specific type.
+	/// @tparam Lhs Type on the left-hand side of the equality operation.
+	/// @tparam Rhs Type on the right-hand side of the equality operation.
+	template <typename T, typename Lhs, typename Rhs = Lhs>
+	concept equality_predicate = requires(const Lhs& lhs, const Rhs& rhs) {
+		{ T{}(lhs, rhs) } -> std::same_as<bool>;
 	};
-	// Concept denoting a "keylike" type for a hash collection.
+
+	/// "Keylike" type compatible with a hash collection.
+	/// @tparam Key Primary key type of the collection.
+	/// @tparam Hash Hasher type of the collection.
+	/// @tparam Eq Equality predicate type of the collection.
 	template <typename T, typename Key, typename Hash, typename Eq>
 	concept hash_keylike = hasher<Hash, T> && equality_predicate<Eq, Key, T>;
 } // namespace tr
