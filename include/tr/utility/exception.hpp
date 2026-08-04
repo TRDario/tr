b2244b18c33508aaf3aa3b99475436ebaa4888a7
TRDario <dario.cvitanovic04@gmail.com>
2026-08-04 19:38:28 +0200

    Document exception.hpp.

--- include/tr/utility/exception.hpp
+++ include/tr/utility/exception.hpp
@@ -1,72 +1,112 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                                                                       //
-// Provides various basic exception types.                                                                                               //
-//                                                                                                                                       //
-// tr::exception provides an interface for tr-style exceptions, which instead of having a single "what" message can consist of up to     //
-// three: name, description, and details. The actual .what() method combines these into the format "<NAME>: <DESCRIPTION> (<DETAILS>)":  //
-//     - tr::custom_exception{"foo error", "bar not found", "baz"}.what() -> "foo error: bar not found (baz)"                            //
-// NOTE: tr::exception uses a single statically allocated buffer for what() string, so it is not possible to have multiple what()        //
-// messages at once.                                                                                                                     //
-//                                                                                                                                       //
-// tr::out_of_memory is a custom out-of-memory exception whose description can be formatted, while tr::custom_exception forwards the     //
-// strings it is passed as the name/description/details strings:                                                                         //
-//     - tr::out_of_memory{"bitmap allocation"}.what() -> "Out of memory: Error occurred during bitmap allocation."                      //
-//     - tr::custom_exception{"foo error", "bar not found", "baz"}.what() -> "foo error: bar not found (baz)"                            //
-//                                                                                                                                       //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file
+/// @brief Provides base exception types.
 
 #pragma once
 #include "static_string.hpp"
 
-//////////////////////////////////////////////////////////////// INTERFACE ////////////////////////////////////////////////////////////////
+//
 
-namespace tr {
-	// Extension of std::exception.
-	class exception : public std::exception {
+namespace tr
+{
+	/// Extension of `std::exception` and base class of tr errors.
+	/// @details
+	/// Instead of having a single "what" message, tr errors are defined with up to three message components: name, description, and
+	/// details. The actual what() method is finalized and combines the three sections into a single message.
+	class exception : public std::exception
+	{
 	  public:
-		// Gets the name of the error.
+		/// @name Information
+		/// @{
+
+		/// Gets the name of the error.
+		/// @return Name of the error.
 		virtual std::string_view name() const = 0;
-		// Gets the description of the error.
+
+		/// Gets the description of the error.
+		/// @return String containing the error description, or empty to omit the description section from the message.
 		virtual std::string_view description() const = 0;
-		// Gets further details about the error.
+
+		/// Gets further details about the error.
+		/// @return String containing details about the error, or empty to omit the details section from the message.
 		virtual std::string_view details() const = 0;
-		// Gets a formatted error message.
+
+		/// Gets a formatted error message.
+		/// @return Error message in the format `"<NAME>: <DESCRIPTION> (<DETAILS>)"`.
 		const char* what() const noexcept final;
 
+		/// @}
+
 	  private:
-		// Buffer used for exception explanation messages.
+		/// Buffer used for exception explanation messages.
 		static thread_local static_string<1024> g_what_buffer;
 	};
 
-	// Custom out-of-memory exception.
-	struct out_of_memory : public exception {
-		// Constructs an out-of-memory exception (arguments are formatted in-place).
-		template <typename... Args> out_of_memory(std::format_string<Args...> fmt, Args&&... args);
+	/// Out-of-memory exception.
+	struct out_of_memory : public exception
+	{
+		/// @name Constructors
+		/// @{
 
-		// Gets the name of the error.
+		/// Constructs an out-of-memory exception (arguments are formatted in-place).
+		/// @tparam Args Formatting argument types.
+		/// @param fmt Error message format string.
+		/// @param args Formatting arguments.
+		template <typename... Args>
+		out_of_memory(std::format_string<Args...> fmt, Args&&... args);
+
+		/// @}
+		/// @name Information
+		/// @{
+
+		/// Gets the name of the error.
+		/// @return `Out of memory`.
 		std::string_view name() const override;
-		// Gets the description of the error.
+
+		/// Gets the description of the error.
+		/// @return Description of the out-of-memory error.
 		std::string_view description() const override;
-		// Gets further details about the error.
+
+		/// Gets further details about the error.
+		/// @return Always empty.
 		std::string_view details() const override;
 
+		/// @}
+
 	  private:
-		// The description of the error.
+		/// Description of the error.
 		static_string<255> m_description;
 	};
 
-	// Specialization of tr::exception for one-off exceptions.
-	class custom_exception : public exception {
+	/// Specialization of tr::exception for one-off exceptions.
+	class custom_exception : public exception
+	{
 	  public:
-		// Constructs an exception.
+		/// @name Constructors
+		/// @{
+
+		/// Constructs an exception.
+		/// @param name Name of the error.
+		/// @param description Description of the error.
+		/// @param details Details of the error.
 		custom_exception(std::string&& name, std::string&& description, std::string&& details);
 
-		// Gets the name of the error.
+		/// @}
+		/// @name Information
+		/// @{
+
+		/// Gets the name of the error.
+		/// @return Name of the error.
 		std::string_view name() const override;
-		// Gets the description of the error.
+
+		/// Gets the description of the error.
+		/// @return Description of the error.
 		std::string_view description() const override;
-		// Gets further details about the error.
+
+		/// Gets further details about the error.
+		/// @return Further details about the error.
 		std::string_view details() const override;
+
+		/// @}
 
 	  private:
 		// The name of the error.
--- include/tr/utility/impl/exception.hpp
+++ include/tr/utility/impl/exception.hpp
@@ -1,15 +1,13 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                                                                       //
-// Implements the templated parts of exception.hpp.                                                                                      //
-//                                                                                                                                       //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file
+/// @brief Implements the templated parts of exception.hpp.
 
 #pragma once
 #include "../exception.hpp"
 
-////////////////////////////////////////////////////////////// OUT OF MEMORY //////////////////////////////////////////////////////////////
+//
 
-template <typename... Args> tr::out_of_memory::out_of_memory(std::format_string<Args...> fmt, Args&&... args)
+template <typename... Args>
+tr::out_of_memory::out_of_memory(std::format_string<Args...> fmt, Args&&... args)
 {
 	auto output_it{std::back_inserter(m_description)};
 	std::format_to(output_it, "Error occurred during ");
