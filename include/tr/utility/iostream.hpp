a881b23aeb6ff4a1d449d43140c2c37271c4de24
TRDario <dario.cvitanovic04@gmail.com>
2026-08-04 19:10:28 +0200

    Document iostream.hpp.

--- include/tr/utility/iostream.hpp
+++ include/tr/utility/iostream.hpp
@@ -1,65 +1,102 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                                                                       //
-// Provides miscellaneous iostream functionality.                                                                                        //
-//                                                                                                                                       //
-// tr::open_file_r and tr::open_file_w act as wrappers over the iostream constructor primarily to provide better error messages:         //
-//     - tr::open_file_w("input.txt") -> std::ofstream to "input.txt"                                                                    //
-//     - tr::open_file_r("nonexistant.txt") -> File not found: nonexistant.txt                                                           //
-//     - tr::open_file_w("restricted.txt") -> File opening error: restricted.txt                                                         //
-//                                                                                                                                       //
-// tr::reached_eof provides a way of checking for end-of-file without having to trigger eofbit or badbit on operations:                  //
-//     - tr::reached_eof(file) -> true, any further operations will set eofbit.                                                          //
-//                                                                                                                                       //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file
+/// @brief Provides stream utilities.
 
 #pragma once
 #include "exception.hpp"
 
-//////////////////////////////////////////////////////////////// INTERFACE ////////////////////////////////////////////////////////////////
+//
 
-namespace tr {
-	// Exception thrown when a file was not found.
-	class file_not_found : public exception {
+namespace tr
+{
+	/// Exception thrown when a file was not found.
+	class file_not_found : public exception
+	{
 	  public:
-		// Constructs an exception.
+		/// @name Constructors
+		/// @{
+
+		/// Constructs an exception.
+		/// @param path Path to the file that wasn't found.
 		file_not_found(std::string_view path);
 
-		// Gets the name of the error.
+		/// @}
+		/// @name Information
+		/// @{
+
+		/// Gets the name of the error.
+		/// @return `"File not found"`.
 		std::string_view name() const override;
-		// Gets the description of the error.
+
+		/// Gets the description of the error.
+		/// @return Path to the file that wasn't found.
 		std::string_view description() const override;
-		// Gets further details about the error.
+
+		/// Gets further details about the error.
+		/// @return Always empty.
 		std::string_view details() const override;
 
+		/// @}
+
 	  private:
-		// Path to the affected file.
+		/// Path to the affected file.
 		std::string m_path;
 	};
-	// Exception thrown when a file opening fails.
-	class file_open_error : public exception {
+
+	/// Exception thrown when a file opening fails.
+	class file_open_error : public exception
+	{
 	  public:
-		// Constructs an exception.
+		/// @name Constructors
+		/// @{
+
+		/// Constructs an exception.
+		/// @param path Path to the file that failed to open.
 		file_open_error(std::string_view path);
 
-		// Gets the name of the error.
+		/// @}
+		/// @name Information
+		/// @{
+
+		/// Gets the name of the error.
+		/// @return `"File opening error"`.
 		std::string_view name() const override;
-		// Gets the description of the error.
+
+		/// Gets the description of the error.
+		/// @return Path to the file whose opening failed.
 		std::string_view description() const override;
-		// Gets further details about the error.
+
+		/// Gets further details about the error.
+		/// @return Always empty.
 		std::string_view details() const override;
 
+		/// @}
+
 	  private:
-		// Path to the affected file.
+		/// Path to the affected file.
 		std::string m_path;
 	};
 
-	// Opens a file for writing with extra checks.
-	// May throw: tr::file_open_error.
+	//
+
+	/// Opens a file for writing with extra checks.
+	/// @param path Path to the file to open.
+	/// @param openmode Stream opening mode.
+	/// @exception file_open_error If opening the file failed.
+	/// @return Output stream to the file at `path`.
 	std::ofstream open_file_w(const std::filesystem::path& path, std::ios::openmode openmode = std::ios::in);
-	// Opens a file for reading with extra checks.
-	// May throw: tr::file_not_found, tr::file_open_error.
+
+	/// Opens a file for reading with extra checks.
+	/// @param path Path to the file to open.
+	/// @param openmode Stream opening mode.
+	/// @exception file_not_found If the file was not found.
+	/// @exception file_open_error If opening the file failed.
+	/// @return Input stream to the file at `path`.
 	std::ifstream open_file_r(const std::filesystem::path& path, std::ios::openmode openmode = std::ios::out);
 
-	// Checks whether a stream has reached end-of-file.
+	//
+
+	/// Checks whether a stream has reached end-of-file.
+	/// @param stream Stream to check for EOF.
+	/// @return `true` if the stream reached EOF, `false` otherwise.
 	bool reached_eof(std::istream& stream);
 } // namespace tr
--- src/utility/iostream.cpp
+++ src/utility/iostream.cpp
@@ -1,12 +1,9 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                                                                       //
-// Implements iostream.hpp.                                                                                                              //
-//                                                                                                                                       //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file
+/// @brief Implements iostream.cpp.
 
 #include "../../include/tr/utility/iostream.hpp"
 
-/////////////////////////////////////////////////////////////// FILE ERRORS ///////////////////////////////////////////////////////////////
+//
 
 tr::file_not_found::file_not_found(std::string_view path)
 	: m_path{std::format("'{}'", path)}
@@ -52,13 +49,13 @@ std::string_view tr::file_open_error::details() const
 	return {};
 }
 
-///////////////////////////////////////////////////////////// STREAM FUNCTIONS ////////////////////////////////////////////////////////////
+//
 
 std::ofstream tr::open_file_w(const std::filesystem::path& path, std::ios::openmode openmode)
 {
 	std::ofstream file{path, openmode};
 	if (!file.is_open()) {
-		throw file_not_found{path.string()};
+		throw file_open_error{path.string()};
 	}
 	file.exceptions(std::ios::badbit | std::ios::failbit);
 	return file;
@@ -76,6 +73,8 @@ std::ifstream tr::open_file_r(const std::filesystem::path& path, std::ios::openm
 	file.exceptions(std::ios::badbit | std::ios::failbit);
 	return file;
 }
+
+//
 
 bool tr::reached_eof(std::istream& stream)
 {
