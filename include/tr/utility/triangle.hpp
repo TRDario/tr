95306e6ce748c4028aae5b6a5ea5d76110bd9d12
TRDario <dario.cvitanovic04@gmail.com>
2026-08-05 16:20:33 +0200

    Document triangle.hpp.

--- include/tr/utility/triangle.hpp
+++ include/tr/utility/triangle.hpp
@@ -1,40 +1,40 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                                                                       //
-// Provides a triangle datatype.                                                                                                         //
-//                                                                                                                                       //
-// A triangle is defined by 3 points designated .a, .b, and .c:                                                                          //
-//     - tr::triangle tri{{500, 500}, {600, 600}, {700, 500}}                                                                            //
-//                                                                                                                                       //
-// The winding order of a triangle may be determined:                                                                                    //
-//     - tri.winding_order() -> tr::winding_order::cw                                                                                    //
-//                                                                                                                                       //
-// Triangles may be checked for containing a point:                                                                                      //
-//     - tri.contains({600, 550}) -> true                                                                                                //
-//                                                                                                                                       //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file
+/// @brief Provides a triangle datatype.
 
 #pragma once
 #include "default_binary_io.hpp"
 #include "polygon.hpp"
 
-//////////////////////////////////////////////////////////////// INTERFACE ////////////////////////////////////////////////////////////////
+//
 
-namespace tr {
-	// 2D triangle.
-	struct triangle {
-		// The first point of the triangle.
+namespace tr
+{
+	/// 2D triangle datatype.
+	struct triangle
+	{
+		/// First point of the triangle.
 		glm::vec2 a;
-		// The second point of the triangle.
+
+		/// Second point of the triangle.
 		glm::vec2 b;
-		// The third point of the triangle.
+
+		/// Third point of the triangle.
 		glm::vec2 c;
 
-		// Determines the winding order of the triangle.
+		//
+
+		/// Determines the winding order of the triangle.
+		/// @return Winding order of the triangle.
 		winding_order winding_order() const;
-		// Determines whether a point is contained inside the triangle.
+
+		/// Determines whether a point is contained inside the triangle.
+		/// @return `true` if `point` is within the triangle, `false` otherwise.
 		bool contains(glm::vec2 point) const;
 	};
 } // namespace tr
 
-// Enables default binary IO for triangles.
-template <> inline constexpr bool tr::enable_default_binary_io<tr::triangle>{true};
+//
+
+/// Enables default binary IO for triangles.
+template <>
+inline constexpr bool tr::enable_default_binary_io<tr::triangle>{true};
