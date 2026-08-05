3f45a63e9fa4034f27716ba57c345d7c707ff05c
TRDario <dario.cvitanovic04@gmail.com>
2026-08-05 16:24:14 +0200

    Document polygon.hpp.

--- include/tr/utility/polygon.hpp
+++ include/tr/utility/polygon.hpp
@@ -1,39 +1,48 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                                                                       //
-// Provides polygon-related datatypes and functions.                                                                                     //
-//                                                                                                                                       //
-// A winding order enumerator is provided (with values cw and ccw).                                                                      //
-//                                                                                                                                       //
-// Polygon views can be checked for their winding order, whether they're simple, whether a point is contained inside then, or whether    //
-// two polygons intersect:                                                                                                               //
-//     - tr::polygon_winding_order(vertices) -> tr::winding_order::cw                                                                    //
-//     - tr::simple_polygon(vertices) -> true                                                                                            //
-//     - tr::point_in_polygon(vertices, {0, 0}) -> false                                                                                 //
-//     - tr::intersecting(vertices1, vertices2) -> true                                                                                  //
-//                                                                                                                                       //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file
+/// @brief Provides polygon-related datatypes and functions.
 
 #pragma once
 #include "common.hpp"
 
-//////////////////////////////////////////////////////////////// INTERFACE ////////////////////////////////////////////////////////////////
+//
 
-namespace tr {
-	// Alias for a view to a polygon.
+namespace tr
+{
+	/// Alias for a view to a polygon's vertices.
 	using polygon_view = std::span<const glm::vec2>;
 
-	// Polygon winding order.
-	enum class winding_order : bool {
-		cw, // Clockwise winding order.
-		ccw // Counter-clockwise winding order.
+	/// Polygon winding order.
+	enum class winding_order : bool
+	{
+		/// Clockwise winding order.
+		cw,
+		/// Counter-clockwise winding order.
+		ccw
 	};
 
-	// Finds the winding order of a polygon.
-	winding_order polygon_winding_order(polygon_view vertices);
-	// Determines whether a polygon is simple (not self-intersecting).
-	bool simple_polygon(polygon_view vertices);
-	// Determines whether a point is contained in a polygon.
-	bool point_in_polygon(glm::vec2 p, polygon_view vertices);
-	// Determines whether two polygons are intersecting.
+	/// @name Polygons
+	/// @{
+
+	/// Finds the winding order of a polygon.
+	/// @param polygon View to a polygon.
+	/// @return Winding order of `polygon`.
+	winding_order polygon_winding_order(polygon_view polygon);
+
+	/// Determines whether a polygon is simple (not self-intersecting).
+	/// @param polygon View to a polygon.
+	/// @return `true` if `polygon` is simple, `false` otherwise.
+	bool simple_polygon(polygon_view polygon);
+
+	/// Determines whether a point is contained in a polygon.
+	/// @param p Point vector.
+	/// @param polygon View to a polygon.
+	/// @return `true` if `p` is within `polygon`, `false` otherwise.
+	bool point_in_polygon(glm::vec2 p, polygon_view polygon);
+
+	/// Determines whether two polygons are intersecting.
+	/// @param a, b Views to polygons.
+	/// @return `true` if `a` and `b` intersect, `false` otherwise.
 	bool intersecting(polygon_view a, polygon_view b);
+
+	/// @}
 } // namespace tr
