91f7398741bddf0d87c9386a7f26618fe065b323
TRDario <dario.cvitanovic04@gmail.com>
2026-08-09 11:44:07 +0200

    Document stopwatch.hpp.

--- include/tr/utility/stopwatch.hpp
+++ include/tr/utility/stopwatch.hpp
@@ -1,35 +1,39 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                                                                       //
-// Provides a simple stopwatch class.                                                                                                    //
-//                                                                                                                                       //
-// Constructing the stopwatch automatically starts it. The stopwatch can then be restarted with the .lap() method, which returns the     //
-// elapsed time of the previous lap. The elapsed time since the stopwatch was started can also be checked at any time with               //
-// the .elapsed() method:                                                                                                                //
-//     - tr::stopwatch stopwatch; -> starts 'stopwatch' automatically                                                                    //
-//     - stopwatch.elapsed() -> gets the elapsed time, but does not restart the stopwatch                                                //
-//     - stopwatch.lap() -> gets the elapsed time and restarts the stopwatch                                                             //
-//                                                                                                                                       //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file
+/// @brief Provides a basic stopwatch class.
 
 #pragma once
 #include "chrono.hpp"
 
-//////////////////////////////////////////////////////////////// INTERFACE ////////////////////////////////////////////////////////////////
+//
 
-namespace tr {
-	// Basic stopwatch class.
-	class stopwatch {
+namespace tr
+{
+	/// Basic stopwatch class.
+	class stopwatch
+	{
 	  public:
-		// Constructs and starts the stopwatch.
+		/// @name Constructors
+		/// @{
+
+		/// Constructs and starts the stopwatch.
 		stopwatch();
 
-		// Gets the elapsed time since the stopwatch was started.
+		/// @}
+		/// @name Access
+		/// @{
+
+		/// Gets the elapsed time since the stopwatch was started.
+		/// @return Elapsed time since the stopwatch was started.
 		duration elapsed() const;
-		// Restarts the stopwatch and gets the elapsed time since it was started.
+
+		/// Restarts the stopwatch and gets the elapsed time since it was started.
+		/// @return Elapsed time of the previous lap.
 		duration lap();
 
+		/// @}
+
 	  private:
-		// The time point at which the current measurement started.
+		/// Time point at which the current measurement started.
 		std::chrono::steady_clock::time_point m_start;
 	};
 } // namespace tr
